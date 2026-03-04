84961fd452ae5f53b87d3d49fa493831309a69f1
Jasper Smet <josbeir@gmail.com>
2026-03-04 18:46:47 +0100

    add sugar trim on scaffold base templates

--- scaffolds/default/templates/layout/base.sugar.php
+++ scaffolds/default/templates/layout/base.sugar.php
@@ -11,7 +11,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="description" content="<?= $site->description ?? 'Starter site scaffolded with Glaze.' ?>" />
-    <title>
+    <title s:trim>
 		<s-ifblock name="title">
 			<s-template s:block="title" /> ·
 		</s-ifblock>
